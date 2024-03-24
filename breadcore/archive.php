4d53e808a31e00fd8c46067030d683316c995dbc
Thomas Wang <tw864@drexel.edu>
2024-03-23 20:30:10 -0400

    updates

--- breadcore/archive.php
+++ breadcore/archive.php
@@ -3,15 +3,18 @@
 <?php
 if (have_posts() ) :
     while (have_posts() ) : the_post();
+        echo '<div class="grid">';
         echo get_the_post_thumbnail( $post_id, 'thumbnail' );
         echo '<h2>' .get_the_title(). '</h2>';
         echo '<p>' .get_the_excerpt(). '</p>';
-        echo '<a href="'.get_the_permalink().'">Read More</a>';
+        echo '<a class="link" href="'.get_the_permalink().'">Read More</a>';
         echo '<hr>';
+        echo '</div>';
     endwhile;
 else:
     echo '<p>No content found</p>';
 endif;
+
 ?>
 </div>
 
