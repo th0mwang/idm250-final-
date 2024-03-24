956da69b9cea0cd2a8c82504a7b901ce6065dbaf
Thomas Wang <tw864@drexel.edu>
2024-03-23 20:30:00 -0400

    updates

--- breadcore/archive.php
+++ breadcore/archive.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
+<div class="blog">
 <?php
-
 if (have_posts() ) :
     while (have_posts() ) : the_post();
         echo get_the_post_thumbnail( $post_id, 'thumbnail' );
@@ -13,5 +13,6 @@ else:
     echo '<p>No content found</p>';
 endif;
 ?>
+</div>
 
 <?php get_footer(); ?>
